a1c31ceca48094f9cfebbf5edfe38f36813a5f4d
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-03-14 22:01:23 +0545

    File updation

--- addFive.js
+++
@@ -1,5 +0,0 @@
-function addFive(num) {
-  return num + 5;
-}
-
-module.exports = addFive;
--- addFive.test.js
+++
@@ -1,5 +0,0 @@
-const addFive = require("./addFive");
-
-test("returns the number plus 5", () => {
-  expect(addFive(1)).toBe(6);
-});
